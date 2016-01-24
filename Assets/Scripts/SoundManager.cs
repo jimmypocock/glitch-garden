86e6370832c9b22c81549454d225a484f3ae97f4
Jimmy Pocock <jimmypocock@yahoo.com>
2016-01-24 15:24:19 -0600

    Created start menu, splash scenes, and asset folders

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+	static SoundManager instance = null;
+
+	public AudioClip splashSound;
+	//	public AudioClip gameClip;
+	//	public AudioClip endClip;
+
+	private AudioSource music;
+
+	void Start ()
+	{
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+
+			music = GetComponent<AudioSource> ();
+			music.clip = splashSound;
+			music.loop = true;
+			music.Play ();
+		}
+
+	}
+
+	void OnLevelWasLoaded (int level)
+	{
+		music.Stop ();
+
+		if (level == 0) {
+			music.clip = splashSound;
+		} else {
+			music.clip = splashSound;
+		}
+		music.loop = true;
+
+		music.Play ();
+	}
+
+}
